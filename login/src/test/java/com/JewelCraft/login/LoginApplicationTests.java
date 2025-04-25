591c3dea632b8599aab3b5bb4f8ca50b876cf95a
Debanjan Paria TPR <debanjan.paria.tpr@in.pwc.com>
2025-04-25 12:13:25 +0530

    initial commit

---
+++ login/src/test/java/com/JewelCraft/login/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JewelCraft.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
